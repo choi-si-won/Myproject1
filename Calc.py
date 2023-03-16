0ce0e2d814b65ec54523cb01d7132f5dabab4523
ChoiSiWon <chltldnjs27@gmail.com>
2023-03-16 14:15:58 +0900

    Commit 1 메인 함수 만듦, 더하기 함수 추가

---
+++ Calc.py
@@ -0,0 +1,16 @@
+## 함수 선언부
+def add_func(n1, n2) :
+    res = n1 + n2
+    return res
+
+
+
+
+
+
+## 전역 변수부
+a, b, res = 14, 27, 0
+
+## 메인 코드부
+res = add_func(a, b)
+print(a, ' + ', b, ' = ', res)
