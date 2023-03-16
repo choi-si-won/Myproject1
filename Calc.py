a21826eda7c104cba889070b904f0b7162ce3c5e
ChoiSiWon <chltldnjs27@gmail.com>
2023-03-16 14:24:24 +0900

    Commit 4 나누기 추가

--- Calc.py
+++ Calc.py
@@ -11,6 +11,10 @@ def dou_func(n1, n2) :
     res = n1 * n2
     return res
 
+def div_func(n1, n2) :
+    res =  n1 / n2
+    return res
+
 ## 전역 변수부
 a, b, res = 14, 27, 0
 
@@ -23,3 +27,6 @@ print(a, ' - ', b, ' = ', res2)
 
 res3 = dou_func(a, b)
 print(a, ' * ', b, ' = ', res3)
+
+res4 = div_func(a, b)
+print(a, ' / ', b, ' = ', res4)
